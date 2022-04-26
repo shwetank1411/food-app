da9e443f1ab3744bf4d8f13088a4b37bc5082a15
shwetank1411 <shwetank.1411@gmail.com>
2022-04-26 11:01:57 +0530

    formatting changes in the app script

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 from items import Items
 from discounts import Discounts
-from orders import Order,AppOrder
-from notifications import Notifications
+from orders import Order, AppOrder
+from notifications import Notifications, Restaurants
 from utilities import PaymentMode
-from notifications import Restaurants
 
 
 def main() -> None:
+    """Whole lifecycle of an order"""
 
-    new_item =  Items('Dosa', 5, 50)
+    # what needs to ordered , and from where --
+    new_item = Items('Dosa', 5, 50)
     restaurant = Restaurants.Swadishtam
     applied_disc = Discounts(discount_coupon='NET05')
 
+    # how the user will pay --
     PaymentMode.about()
     user_payment_mode = PaymentMode.net
     print(f"selected payment mode is -- {user_payment_mode.value}")
 
+    # new order creation --
     new_order: Order = AppOrder(order_items=new_item, order_restaurant=restaurant, order_discounts=applied_disc)
-
     info = new_order.order_info()
     print(info)
+
+    # order verification --
     new_order.verify_order()
+
+    # order payment
     new_order.order_payment(user_payment_mode)
+
+    # order notification to restaurant
     Notifications.notify_restaurant(new_order.order_restaurant)
+
+    # order cancellation
     new_order.cancel_order()
 
 
