5c6c42fc3f2b4bc0dc0f574f90ea950f7513ae29
shwetank1411 <shwetank.1411@gmail.com>
2022-04-26 08:02:52 +0530

    added code to call payment mode about function call

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def main() ->None :
     new_item =  Items('Dosa',5,50)
     restaurant = Restaurants.Swadishtam
     applied_disc = Discounts(discount_coupon='NET05')
+
+    PaymentMode.about()
     user_payment_mode = PaymentMode.net
 
     new_order :Order = AppOrder(order_items=new_item,order_restaurant=restaurant,order_discounts=applied_disc)
